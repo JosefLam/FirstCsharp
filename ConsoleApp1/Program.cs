7ee9ee96729acf6d95662e3fb6b010f6695f007d
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-09-11 21:15:07 +0100

    update adds options

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,9 +12,17 @@ namespace ConsoleApp1
         {
             Console.WriteLine("how many pizzas?");
             int answer = int.Parse(Console.ReadLine());
-            if (answer < 1)
+            if (answer >1)
             {
                 Console.WriteLine("Youre going to put on weight");
+            }
+            if (answer==1)
+            {
+                Console.WriteLine("thats pretty standard");
+            }
+            if (answer <=0)
+            {
+                Console.WriteLine("you need to eat something");
             }
 
             Console.ReadLine();
